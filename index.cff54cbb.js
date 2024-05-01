16e58600052f77d83eca4e94fb9a74bf22982896
KhailoAndrey <110947394+KhailoAndrey@users.noreply.github.com>
2024-05-01 09:57:00 +0000

    Deploying to gh-pages from @ KhailoAndrey/ZooClinic@b1de8ee221293c18f3c8648ff9986128828b1542 🚀

--- index.cff54cbb.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".banner__slider",{loop:!0,initialSlide:0,slidesPerView:1,pagination:{el:".swiper-pagination",clickable:!0},autoplay:{delay:1e3,stopOnLastSlide:!0,disableOnInteraction:!1},speed:3e3,effect:"fade",crossFade:!0});
-//# sourceMappingURL=index.cff54cbb.js.map
