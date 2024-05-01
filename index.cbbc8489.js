487aea444335871657788a5612e4767e4ee55731
KhailoAndrey <110947394+KhailoAndrey@users.noreply.github.com>
2024-05-01 11:37:42 +0000

    Deploying to gh-pages from @ KhailoAndrey/ZooClinic@ca099381a4f80f5a043ed3d88cc0e801050b154a 🚀

---
+++ index.cbbc8489.js
@@ -0,0 +1,2 @@
+window.changeLanguage=function(e){document.querySelectorAll(".language-switcher button").forEach(function(e){e.classList.remove("active")}),e.classList.add("active")};
+//# sourceMappingURL=index.cbbc8489.js.map
