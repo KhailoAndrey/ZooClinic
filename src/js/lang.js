d6922ccdd5c7b057b8d2ec3e68e10856d001db27
KhailoAndrey <hap512@gmail.com>
2024-06-17 02:09:31 +0300

    add mob header

--- src/js/lang.js
+++ src/js/lang.js
@@ -1,9 +1,16 @@
 export function changeLanguage(button) {
-    var buttons = document.querySelectorAll('.language-switcher button');
-    buttons.forEach(function(btn) {
-        btn.classList.remove('active');
-    });
-    button.classList.add('active');
+  var buttons = document.querySelectorAll('.language-switcher button');
+  buttons.forEach(function (btn) {
+    btn.classList.remove('active');
+  });
+  button.classList.add('active');
 }
 
 window.changeLanguage = changeLanguage;
+
+document
+  .querySelector('.lang-switch-btn')
+  .addEventListener('click', function () {
+    var span = this.querySelector('span');
+    span.textContent = span.textContent === 'RU' ? 'UA' : 'RU';
+  });
