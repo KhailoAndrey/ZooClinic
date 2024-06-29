989e781e8dcd9f19dadedea8f714f20c281f7647
KhailoAndrey <hap512@gmail.com>
2024-06-29 12:29:03 +0300

    add slide3 mob

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const swiper = new Swiper('.slider', {
   direction: 'vertical',
   loop: true,
   // loopAdditionalSlides: 0,
-  speed: 4000,
+  speed: 5000,
   autoplay: false,
   // autoplay: {
   //   delay: 5000,
@@ -35,13 +35,13 @@ const swiper = new Swiper('.slider', {
     nextEl: '.btnY',
   },
 });
-setTimeout(() => {
-  swiper.params.autoplay = {
-    delay: 5000, // Устанавливаем задержку между сменой слайдов
-    disableOnInteraction: false,
-  };
-  swiper.autoplay.start(); // Запускаем автоплей
-}, 10000); // 10 секунд
+// setTimeout(() => {
+//   swiper.params.autoplay = {
+//     delay: 5000, // Устанавливаем задержку между сменой слайдов
+//     disableOnInteraction: false,
+//   };
+//   swiper.autoplay.start(); // Запускаем автоплей
+// }, 10000); // 10 секунд
 const slideNumber = 3;
 const nextButtons = document.querySelectorAll('.moreBtn');
 nextButtons.forEach(button => {
